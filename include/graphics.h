e8767f5f483226ec86607932f37d2c7bad5e9d64
Andrea Giove <andreagiove@outlook.com>
2020-12-21 19:05:50 +0100

    added draw_point function

--- include/graphics.h
+++ include/graphics.h
@@ -23,6 +23,7 @@ void rotate_point(float* x, float* y, float xc, float yc,
 	float cos_angle, float sin_angle);
 void convert_coord_to_display(int src_x, int src_y, int* dst_x, int* dst_y);
 void draw_airplane(const airplane_t* airplane, int color);
+void draw_point(const waypoint_t* point, int color);
 void draw_trail(const cbuffer_t* trails, int n, int color);
 void handle_airplane_trail(const airplane_t* airplane, cbuffer_t* trail);
 
